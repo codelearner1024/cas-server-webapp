c4a6e0c01e93bf10eeacb5bda64e3b1b4bf65ba3
codelearner1024 <wuweigang11@outlook.com>
2018-06-10 17:51:56 +0800

    添加内登录逻辑，明日测试

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -16,149 +16,167 @@ import java.util.regex.Pattern;
  */
 public class Client {
 
-    public static String getTicket(final String server, final String username, final String password,
-                                   final String service) {
-        notNull(server, "server must not be null");
-        notNull(username, "username must not be null");
-        notNull(password, "password must not be null");
-        notNull(service, "service must not be null");
-        return getServiceTicket(server, getTicketGrantingTicket(server, username, password), service);
-    }
+	public static String getTicket(final String server, final String username, final String password,
+			final String service) {
+		notNull(server, "server must not be null");
+		notNull(username, "username must not be null");
+		notNull(password, "password must not be null");
+		notNull(service, "service must not be null");
+		return getServiceTicket(server, getTicketGrantingTicket(server, username, password), service);
+	}
 
-    /**
-     * 取得ST
-     *
-     * @param server
-     * @param ticketGrantingTicket
-     * @param service
-     */
-    private static String getServiceTicket(final String server, final String ticketGrantingTicket, final String service) {
-        if (ticketGrantingTicket == null)
-            return null;
+	/**
+	 * 取得ST
+	 *
+	 * @param server
+	 * @param ticketGrantingTicket
+	 * @param service
+	 */
+	private static String getServiceTicket(final String server, final String ticketGrantingTicket,
+			final String service) {
+		if (ticketGrantingTicket == null)
+			return null;
 
-        final HttpClient client = new HttpClient();
+		final HttpClient client = new HttpClient();
 
-        final PostMethod post = new PostMethod(server + "/" + ticketGrantingTicket);
+		final PostMethod post = new PostMethod(server + "/" + ticketGrantingTicket);
 
-        post.setRequestBody(new NameValuePair[]{new NameValuePair("service", service)});
+		post.setRequestBody(new NameValuePair[] { new NameValuePair("service", service) });
 
-        try {
-            client.executeMethod(post);
+		try {
+			client.executeMethod(post);
 
-            final String response = post.getResponseBodyAsString();
+			final String response = post.getResponseBodyAsString();
 
-            switch (post.getStatusCode()) {
-                case 200:
-                    return response;
+			switch (post.getStatusCode()) {
+			case 200:
+				return response;
 
-                default:
-                    warning("Invalid response code (" + post.getStatusCode() + ") from CAS server!");
-                    info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));
-                    break;
-            }
-        } catch (final IOException e) {
-            warning(e.getMessage());
-        } finally {
-            post.releaseConnection();
-        }
+			default:
+				warning("Invalid response code (" + post.getStatusCode() + ") from CAS server!");
+				info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));
+				break;
+			}
+		} catch (final IOException e) {
+			warning(e.getMessage());
+		} finally {
+			post.releaseConnection();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    /**
-     * @param server
-     * @param username
-     * @param password
-     * 获取服务端的授权
-     */
-    private static String getTicketGrantingTicket(final String server, final String username, final String password) {
-        final HttpClient client = new HttpClient();
+	/**
+	 * @param server
+	 * @param username
+	 * @param password
+	 *            获取服务端的授权（获取TGT）
+	 */
+	private static String getTicketGrantingTicket(final String server, final String username, final String password) {
+		final HttpClient client = new HttpClient();
 
-        final PostMethod post = new PostMethod(server);
+		final PostMethod post = new PostMethod(server);
 
-        post.setRequestBody(new NameValuePair[]{new NameValuePair("username", username),
-                new NameValuePair("password", password)});
+		post.setRequestBody(new NameValuePair[] { new NameValuePair("username", username),
+				new NameValuePair("password", password) });
 
-        try {
-            client.executeMethod(post);
+		try {
+			client.executeMethod(post);
 
-            final String response = post.getResponseBodyAsString();
-            info("TGT=" + response);
-            switch (post.getStatusCode()) {
-                case 201: {
-                    final Matcher matcher = Pattern.compile(".*action=\".*/(.*?)\".*").matcher(response);
+			final String response = post.getResponseBodyAsString();
+			info("TGT=" + response);
+			switch (post.getStatusCode()) {
+			case 201: {
+				final Matcher matcher = Pattern.compile(".*action=\".*/(.*?)\".*").matcher(response);
 
-                    if (matcher.matches())
-                        return matcher.group(1);
+				if (matcher.matches())
+					return matcher.group(1);
 
-                    warning("Successful ticket granting request, but no ticket found!");
-                    info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));
-                    break;
-                }
+				warning("Successful ticket granting request, but no ticket found!");
+				info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));
+				break;
+			}
 
-                default:
-                    warning("Invalid response code (" + post.getStatusCode() + ") from CAS server!");
-                    info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));
-                    break;
-            }
-        } catch (final IOException e) {
-            warning(e.getMessage());
-        } finally {
-            post.releaseConnection();
-        }
+			default:
+				warning("Invalid response code (" + post.getStatusCode() + ") from CAS server!");
+				info("Response (1k): " + response.substring(0, Math.min(1024, response.length())));
+				break;
+			}
+		} catch (final IOException e) {
+			warning(e.getMessage());
+		} finally {
+			post.releaseConnection();
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private static void ticketValidate(String serverValidate, String serviceTicket, String service) {
-        notNull(serviceTicket, "paramter 'serviceTicket' is not null");
-        notNull(service, "paramter 'service' is not null");
+	private static void ticketValidate(String serverValidate, String serviceTicket, String service) {
+		notNull(serviceTicket, "paramter 'serviceTicket' is not null");
+		notNull(service, "paramter 'service' is not null");
 
-        final HttpClient client = new HttpClient();
-        GetMethod post = null;
+		final HttpClient client = new HttpClient();
+		GetMethod post = null;
 
-        try {
-            post = new GetMethod(serverValidate + "?" + "ticket=" + serviceTicket + "&service=" + URLEncoder.encode(service, "UTF-8"));
-            client.executeMethod(post);
+		try {
+			post = new GetMethod(serverValidate + "?" + "ticket=" + serviceTicket + "&service="
+					+ URLEncoder.encode(service, "UTF-8"));
+			client.executeMethod(post);
 
-            final String response = post.getResponseBodyAsString();
-            info(response);
-            switch (post.getStatusCode()) {
-                case 200: {
-                    info("成功取得用户数据");
-                }
-                default: {
-                }
-            }
+			final String response = post.getResponseBodyAsString();
+			info(response);
+			switch (post.getStatusCode()) {
+			case 200: {
+				info("成功取得用户数据");
+			}
+			default: {
+			}
+			}
 
-        } catch (Exception e) {
-            warning(e.getMessage());
-        } finally {
-            //释放资源
-            post.releaseConnection();
-        }
+		} catch (Exception e) {
+			warning(e.getMessage());
+		} finally {
+			// 释放资源
+			post.releaseConnection();
+		}
 
-    }
+	}
 
-    private static void notNull(final Object object, final String message) {
-        if (object == null)
-            throw new IllegalArgumentException(message);
-    }
+	private static void notNull(final Object object, final String message) {
+		if (object == null)
+			throw new IllegalArgumentException(message);
+	}
 
-    public static void main(final String[] args) throws Exception {
-        final String server = "http://localhost:8080/cas/v1/tickets";
-        final String username = "adminoue";
-        final String password = "123456";
-        final String service = "http://localhost:8888/index";
-        final String proxyValidate = "http://localhost:8080/cas/proxyValidate";
-        ticketValidate(proxyValidate, getTicket(server, username, password, service), service);
-    }
+	/**  
+	 * @param proxyValidate
+	 * @param server
+	 * @param username
+	 * @param password
+	 * @param service  
+	 * @功能描述:内登录
+	 * @Author:WWG
+	 * @date:2018年6月10日  下午5:21:44
+	 * @Version:1.0  
+	 */
+	public static void intenerLogin(String proxyValidate, String server, String username, String password,
+			String service) {
+		ticketValidate(proxyValidate, getTicket(server, username, password, service), service);
+	}
 
-    private static void warning(String msg) {
-        System.out.println(msg);
-    }
+	public static void main(final String[] args) throws Exception {
+		final String server = "http://localhost:8080/cas/v1/tickets";
+		final String username = "adminoue";
+		final String password = "123456";
+		final String service = "http://localhost:8888/index";
+		final String proxyValidate = "http://localhost:8080/cas/proxyValidate";
+		ticketValidate(proxyValidate, getTicket(server, username, password, service), service);
+	}
 
-    private static void info(String msg) {
-        System.out.println(msg);
-    }
+	private static void warning(String msg) {
+		System.out.println(msg);
+	}
+
+	private static void info(String msg) {
+		System.out.println(msg);
+	}
 }
